86e8327baa75f4f09261482098c2aa4ee3e929ad
mdanamulhasan201 <anamul15-3625@diu.edu.bd>
2025-01-07 08:31:49 +0600

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,22 @@ import logo from '../assets/images/logo.png'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear(); // Get the current year dynamically
+  const currentYear = new Date().getFullYear() // Get the current year dynamically
 
   return (
     <div className='bg-[#1C140A] text-white font-aeonik'>
       <div className='container px-8 xl:px-0 py-24'>
-        <div className='flex flex-col md:flex-row justify-between'>
-          {/* left side */}
-          <div className='w-6/12'>
+        <div className='grid grid-cols-4 md:grid-cols-12 gap-10 md:gap-6'>
+          {/* Left Side */}
+          <div className='col-span-2 md:col-span-6'>
             <div className='w-[145px]'>
-              <img src={logo} alt='' className='w-full' />
+              <img src={logo} alt='Logo' className='w-full' />
             </div>
             <h3 className='text-[14px] text-[#ffffffc5] mt-4'>
               Elevate Your Career
             </h3>
             <div className='mt-5'>
-              <p className='capitalize text-[14px] text-[#ffffffc5]'>email:</p>
+              <p className='capitalize text-[14px] text-[#ffffffc5]'>Email:</p>
               <p>info@cifa.ac</p>
               <div className='flex items-center gap-3 mt-5'>
                 <FaLinkedin className='text-xl text-[#ffffffa8]' />
@@ -27,78 +27,85 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          {/* right side */}
-          <div className='w-6/12'>
-            <div className='flex flex-wrap justify-between'>
-              <div className='flex flex-col  gap-5'>
-                <h2 className='capitalize text-[16px] text-[#ffffffde] font-semibold'>
-                  menu
-                </h2>
-                <ul className='flex flex-col gap-5 font-[14px] text-[#ffffffc0] capitalize'>
-                  <li>
-                    <Link>Home</Link>
-                  </li>
-                  <li>
-                    <Link>instructors</Link>
-                  </li>
-                  <li>
-                    <Link>courses</Link>
-                  </li>
-                  <li>
-                    <Link>exams</Link>
-                  </li>
-                  <li>
-                    <Link>price list</Link>
-                  </li>
-                </ul>
-              </div>
-              <div className='flex flex-col gap-5'>
-                <h2 className='capitalize text-[16px] text-[#ffffffde] font-semibold'>
-                  menu
-                </h2>
-                <ul className='flex flex-col gap-5 font-[14px] text-[#ffffffc0] capitalize'>
-                  <li>
-                    <Link>accreditation</Link>
-                  </li>
-                  <li>
-                    <Link>Employability Tools</Link>
-                  </li>
-                  <li>
-                    <Link>hire candidates</Link>
-                  </li>
-                  <li>
-                    <Link>corporate tools</Link>
-                  </li>
-                </ul>
-              </div>
-              <div className='flex flex-col gap-5'>
-                <h2 className='capitalize text-[16px] text-[#ffffffde] font-semibold'>
-                  menu
-                </h2>
-                <ul className='flex flex-col gap-5 font-[14px] text-[#ffffffc0] capitalize'>
-                  <li>
-                    <Link>Home</Link>
-                  </li>
-                  <li>
-                    <Link>university enrolment</Link>
-                  </li>
-                  <li>
-                    <Link>about CIFA</Link>
-                  </li>
-                  <li>
-                    <Link>FAQs</Link>
-                  </li>
-                </ul>
-              </div>
+
+          {/* Menu 1 */}
+          <div className='col-span-2 md:col-span-2'>
+            <div className='flex flex-col gap-5'>
+              <h2 className='capitalize text-[16px] text-[#ffffffde] font-semibold'>
+                Menu
+              </h2>
+              <ul className='flex flex-col gap-5 text-[14px] text-[#ffffffc0] capitalize whitespace-nowrap'>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/instructors'>Instructors</Link>
+                </li>
+                <li>
+                  <Link to='/courses'>Courses</Link>
+                </li>
+                <li>
+                  <Link to='/exams'>Exams</Link>
+                </li>
+                <li>
+                  <Link to='/price-list'>Price List</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Menu 2 */}
+          <div className='col-span-2 md:col-span-2'>
+            <div className='flex flex-col gap-5'>
+              <h2 className='capitalize text-[16px] text-[#ffffffde] font-semibold'>
+                Menu
+              </h2>
+              <ul className='flex flex-col gap-5 text-[14px] text-[#ffffffc0] capitalize whitespace-nowrap'>
+                <li>
+                  <Link to='/accreditation'>Accreditation</Link>
+                </li>
+                <li>
+                  <Link to='/employability-tools'>Employability Tools</Link>
+                </li>
+                <li>
+                  <Link to='/hire-candidates'>Hire Candidates</Link>
+                </li>
+                <li>
+                  <Link to='/corporate-tools'>Corporate Tools</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Menu 3 */}
+          <div className='col-span-2 md:col-span-2'>
+            <div className='flex flex-col gap-5'>
+              <h2 className='capitalize text-[16px] text-[#ffffffde] font-semibold'>
+                Menu
+              </h2>
+              <ul className='flex flex-col gap-5 text-[14px] text-[#ffffffc0] capitalize whitespace-nowrap'>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/university-enrolment'>University Enrolment</Link>
+                </li>
+                <li>
+                  <Link to='/about-cifa'>About CIFA</Link>
+                </li>
+                <li>
+                  <Link to='/faqs'>FAQs</Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
 
         {/* sub footer */}
-        <div className='border-t mt-20 border-[#ffffff33]'>
+        <div className='md:border-t mt-20 md:border-[#ffffff33]'>
           <div className='mt-10'>
-            <div className='flex justify-between items-center'>
-              <ul className='flex flex items-center gap-5 text-[#ffffffc0] font-aeonik'>
+            <div className='flex flex-col md:flex-row justify-between items-center'>
+              <ul className='grid grid-cols-2 md:flex   items-center gap-10 text-[#ffffffc0] font-aeonik whitespace-nowrap'>
                 <li>
                   <Link>Terms of Use</Link>
                 </li>
@@ -112,7 +119,7 @@ const Footer = () => {
                   <Link>Privacy Policy</Link>
                 </li>
               </ul>
-              <p className='text-[#ffffffc0] font-aeonik'>
+              <p className='text-[#ffffffc0] font-aeonik border-[#ffffff33] border-t md:border-none w-full md:text-end text-center py-5 md:py-0 md:mt-0 mt-10'>
                 © {currentYear} CIFA. All rights reserved.
               </p>
             </div>
@@ -120,7 +127,7 @@ const Footer = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
